a66bf354d5e53b2c4739bfa2f918c83f0c326055
techno-express <technoexpressnet@gmail.com>
2020-11-21 13:14:46 -0500

    update allow `run` and `path` functions to conditional override binary location for sfx methods

--- util/path.js
+++ util/path.js
@@ -1,18 +1,18 @@
 'use strict';
 const path = require("path");
 
-module.exports = function (options) {
-    // Create a string that can be parsed by `run`.
-    let type = typeof options;
-    if ((options) && (type == "object") && (options.hasOwnProperty('path'))) {
-        return options.path;
-    } else {
-        let binaryPath = path.join(__dirname, "..", "binaries", process.platform);
-        let binaryFilename = (process.platform == "win32") ? '7za.exe' : '7za';
-        return {
-            path: binaryPath,
-            filename: binaryFilename,
-            filepath: path.join(binaryPath, binaryFilename)
-        }
+module.exports = function (options, override = false) {
+  // Create a string that can be parsed by `run`.
+  let type = typeof options;
+  if ((options) && (type == "object") && (options.hasOwnProperty('path'))) {
+    return options.path;
+  } else {
+    let binaryPath = path.join(__dirname, "..", "binaries", (override == true ? process.platform + sep + 'other32' : process.platform));
+    let binaryFilename = (process.platform == "win32") ? '7za.exe' : '7za';
+    return {
+      path: binaryPath,
+      filename: binaryFilename,
+      filepath: path.join(binaryPath, binaryFilename)
     }
+  }
 };
--- util/run.js
+++ util/run.js
@@ -1,10 +1,10 @@
 'use strict';
 const os = require('os'),
-    spawn = require('cross-spawn'),
-    when = require('when'),
-    path = require('path'),
-    utilSwitches = require('./switches'),
-    _7zPath = require('./path');
+  spawn = require('cross-spawn'),
+  when = require('when'),
+  path = require('path'),
+  utilSwitches = require('./switches'),
+  _7zPath = require('./path');
 
 /**
  * @promise Run
@@ -14,99 +14,103 @@ const os = require('os'),
  * @reject {Error} The error issued by 7-Zip.
  * @reject {number} Exit code issued by 7-Zip.
  */
-module.exports = function (command, switches) {
-    return when.promise(function (fulfill, reject, progress) {
+module.exports = function (command, switches, override = false) {
+  return when.promise(function (fulfill, reject, progress) {
 
-        // Parse the command variable. If the command is not a string reject the
-        // Promise. Otherwise transform the command into two variables: the command
-        // name and the arguments.
-        if (typeof command !== 'string') {
-            return reject(new Error('Command must be a string'));
-        }
-        // add platform binary to command
-        let pathTo7z = _7zPath();
-        let tmpCmd = command.split(' ')[0];
-        let cmd = path.join(pathTo7z.path, tmpCmd);
-        let args = [command.split(' ')[1]];
+    // Parse the command variable. If the command is not a string reject the
+    // Promise. Otherwise transform the command into two variables: the command
+    // name and the arguments.
+    if (typeof command !== 'string') {
+      return reject(new Error('Command must be a string'));
+    }
+    // add platform binary to command
+    let pathTo7z = _7zPath({}, override);
+    let tmpCmd = command.split(' ')[0];
+    let cmd = path.join(pathTo7z.path, tmpCmd);
+    let args = [command.split(' ')[1]];
 
-        // Parse and add command (non-switches parameters) to `args`.
-        let regexpCommands = /"((?:\\.|[^"\\])*)"/g;
-        let commands = command.match(regexpCommands);
-        if (commands) {
-            commands.forEach(function (c) {
-                c = c.replace(/\//g, path.sep);
-                c = c.replace(/\\/g, path.sep);
-                c = path.normalize(c);
-                args.push(c);
-            });
-        }
+    // Parse and add command (non-switches parameters) to `args`.
+    let regexpCommands = /"((?:\\.|[^"\\])*)"/g;
+    let commands = command.match(regexpCommands);
+    if (commands) {
+      commands.forEach(function (c) {
+        c = c.replace(/\//g, path.sep);
+        c = c.replace(/\\/g, path.sep);
+        c = path.normalize(c);
+        args.push(c);
+      });
+    }
 
-        // Special treatment for the output switch because it is exposed as a
-        // parameter in the API and not as a option. Plus wilcards can be passed.
-        let regexpOutput = /-o"((?:\\.|[^"\\])*)"/g;
-        let output = command.match(regexpOutput);
-        if (output) {
-            args.pop();
-            let o = output[0];
-            o = o.replace(/\//g, path.sep);
-            o = o.replace(/\\/g, path.sep);
-            o = o.replace(/"/g, '');
-            o = path.normalize(o);
-            args.push(o);
-        }
+    // Special treatment for the output switch because it is exposed as a
+    // parameter in the API and not as a option. Plus wilcards can be passed.
+    let regexpOutput = /-o"((?:\\.|[^"\\])*)"/g;
+    let output = command.match(regexpOutput);
+    if (output) {
+      args.pop();
+      let o = output[0];
+      o = o.replace(/\//g, path.sep);
+      o = o.replace(/\\/g, path.sep);
+      o = o.replace(/"/g, '');
+      o = path.normalize(o);
+      args.push(o);
+    }
 
-        // Add switches to the `args` array.
-        var switchesArray = utilSwitches(switches);
-        switchesArray.forEach(function (s) { args.push(s); });
-
-        // Remove now double quotes. If present in the spawned process 7-Zip will
-        // read them as part of the paths (e.g.: create a `"archive.7z"` with
-        // quotes in the file-name);
-        args.forEach(function (e, i) {
-            if (typeof e !== 'string') {
-                return;
-            }
-            if (e.substr(0, 1) !== '-') {
-                e = e.replace(/^"/, '');
-                e = e.replace(/"$/, '');
-                args[i] = e;
-            }
-        });
-
-        // Add bb2 to args array so we get file info
-        args.push('-bb2');
-
-        // When an stdout is emitted, parse it. If an error is detected in the body
-        // of the stdout create an new error with the 7-Zip error message as the
-        // error's message. Otherwise progress with stdout message.
-        let err;
-        let reg = new RegExp('Error:(' + os.EOL + '|)?(.*)', 'i');
-        let res = {
-            cmd: cmd,
-            args: args,
-            options: { stdio: 'pipe' }
-        };
-
-        //console.log('>> ', res.cmd, res.args.join(' '), res.options,' <<');
-        let run = spawn(res.cmd, res.args, res.options);
-        run.stderr.on('data', function (data) {
-            let res = reg.exec(data.toString());
-            if (res) {
-                err = new Error(res[2].substr(0, res[2].length - 1));
-            }
-        });
-        run.stdout.on('data', function (data) {
-            return progress(data.toString());
-        });
-        run.on('error', function (err) {
-            reject(err);
-        });
-        run.on('close', function (code) {
-            if (code === 0) {
-                return fulfill(args);
-            }
-            return reject(err, code);
-        });
-
+    // Add switches to the `args` array.
+    var switchesArray = utilSwitches(switches);
+    switchesArray.forEach(function (s) {
+      args.push(s);
     });
+
+    // Remove now double quotes. If present in the spawned process 7-Zip will
+    // read them as part of the paths (e.g.: create a `"archive.7z"` with
+    // quotes in the file-name);
+    args.forEach(function (e, i) {
+      if (typeof e !== 'string') {
+        return;
+      }
+      if (e.substr(0, 1) !== '-') {
+        e = e.replace(/^"/, '');
+        e = e.replace(/"$/, '');
+        args[i] = e;
+      }
+    });
+
+    // Add bb2 to args array so we get file info
+    args.push('-bb2');
+
+    // When an stdout is emitted, parse it. If an error is detected in the body
+    // of the stdout create an new error with the 7-Zip error message as the
+    // error's message. Otherwise progress with stdout message.
+    let err;
+    let reg = new RegExp('Error:(' + os.EOL + '|)?(.*)', 'i');
+    let res = {
+      cmd: cmd,
+      args: args,
+      options: {
+        stdio: 'pipe'
+      }
+    };
+
+    //console.log('>> ', res.cmd, res.args.join(' '), res.options,' <<');
+    let run = spawn(res.cmd, res.args, res.options);
+    run.stderr.on('data', function (data) {
+      let res = reg.exec(data.toString());
+      if (res) {
+        err = new Error(res[2].substr(0, res[2].length - 1));
+      }
+    });
+    run.stdout.on('data', function (data) {
+      return progress(data.toString());
+    });
+    run.on('error', function (err) {
+      reject(err);
+    });
+    run.on('close', function (code) {
+      if (code === 0) {
+        return fulfill(args);
+      }
+      return reject(err, code);
+    });
+
+  });
 };
